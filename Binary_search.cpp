852620b2bd1a1f8f25825b713b9713cc71dca802
Agyanii <72977044+Agyanii@users.noreply.github.com>
2020-10-17 09:59:01 +0530

    Create Binary_search.cpp

---
+++ Binary_search.cpp
@@ -0,0 +1,27 @@
+#include"bits/stdc++.h"
+using namespace std;
+
+int main() {
+	int a[] = {2, 9 ,3, 1, 4 , 5, 6 ,8 ,7};
+	int n = sizeof(a) / sizeof(a[0]);
+	sort(a, a + n);
+	int l = 0, r = n - 1;
+	
+	bool f = false;
+	while (l < r ) {
+		int m = l + (r - l) / 2;
+		if(a[m] == target) {
+			f = true;
+			break;
+		}
+		else if(a[m] < target) {
+			r = m - 1;
+		}
+		else l = m + 1;
+	}
+	
+	if(f) cout << "Element found at index : "	 << m << "\n";
+	else cout << "Element isn't in the list\n";
+	
+   	return 0;
+}
